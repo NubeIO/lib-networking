c122c1722b05620fa1c57a782a0802d286c5bdee
NubeDev <aidan@enviroservices.com.au>
2022-08-21 12:14:19 +1000

    fix helper

--- ip/checks.go
+++ ip/checks.go
@@ -38,7 +38,7 @@ func (inst *IPv4) IsIPSubnet(address string) (bool, error) {
 	mask := net.IPMask(net.ParseIP(address).To4()) // If you have the mask as a string
 	prefixSize, _ := mask.Size()
 	if prefixSize == 0 {
-		return false, errors.New("invalid subnet address, 255.255.255.0")
+		return false, errors.New("invalid subnet address try, 255.255.255.0")
 	}
 	return true, nil
 }
--- ip/iphelpers_test.go
+++ ip/iphelpers_test.go
@@ -8,6 +8,6 @@ import (
 func TestNetIP(*testing.T) {
 
 	fmt.Println(GetIPSubnet("192.168.15.10", "255.255.255.0"))
-	fmt.Println(SubnetString(6))
+	fmt.Println(New().IsIPSubnet("255.255.2550"))
 
 }
