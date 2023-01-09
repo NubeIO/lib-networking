61d5ea238359badb8d178b5ad61f73ed819a9fc8
NubeDev <aidan@enviroservices.com.au>
2023-01-10 09:09:46 +1100

    update get gateway ip

--- networking/gateway.go
+++ networking/gateway.go
@@ -4,20 +4,32 @@ import (
 	"github.com/jackpal/gateway"
 	"net"
 	"os/exec"
+	"runtime"
 	"strings"
 )
 
 //GetGatewayIP Get gateway IP address
 func (inst *nets) GetGatewayIP(iFaceName string) (ip string, err error) {
-	discoverInterface, err := gateway.DiscoverGateway()
-	if err != nil {
-		return "", err
+	if runtime.GOOS == "linux" {
+		return inst.GetGatewayIPLinux(iFaceName)
+	} else {
+		discoverInterface, err := gateway.DiscoverGateway()
+		if err != nil {
+			return "", err
+		}
+		return discoverInterface.String(), nil
 	}
-	return discoverInterface.String(), nil
 }
 
 //GetGatewayIPLinux Get gateway IP address
 func (inst *nets) GetGatewayIPLinux(iFaceName string) (ip string, err error) {
+	if iFaceName == "" {
+		discoverInterface, err := gateway.DiscoverGateway()
+		if err != nil {
+			return "", err
+		}
+		return discoverInterface.String(), nil
+	}
 	cmd := exec.Command("ip", "route", "show", "dev", iFaceName)
 	d, err := cmd.Output()
 	if err != nil || cmd.ProcessState.ExitCode() != 0 {
